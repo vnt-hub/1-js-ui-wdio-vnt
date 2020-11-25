896bf2aff4ad678cecdd6e8dfae6c4d1fd3218b4
vnt-hub <69205626+vnt-hub@users.noreply.github.com>
2020-11-25 23:01:30 +0300

    ts support

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,3 +1,5 @@
+require("ts-node").register({ files: true });
+
 exports.config = {
     //
     // ====================
@@ -17,7 +19,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/specs/**/*.js'
+        './test/specs/**/*.ts'
     ],
     // Patterns to exclude.
     exclude: [

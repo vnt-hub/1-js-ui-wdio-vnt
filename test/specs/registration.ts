6bd0bb44d12abd97c965ff9d0be610872c4babc4
vnt-hub <69205626+vnt-hub@users.noreply.github.com>
2020-12-01 16:00:07 +0300

    reg

---
+++ test/specs/registration.ts
@@ -0,0 +1,29 @@
+import * as assert from 'assert';
+
+describe ('User', function () {
+    it ('can register', function () {
+        browser.url(`/signup`)
+
+        const email = `test${new Date().getTime() / 1000}@test.com`
+
+        $('#user_name').setValue(email)
+
+        $('#user_email').setValue(email)
+
+        $('#user_password').setValue(email)
+        $('#user_password_confirmation').setValue(email)
+
+        $('input[name=commit]').click()
+
+        browser.pause(2000)
+        const alert = $('.alert-success')
+
+        assert(alert.isDisplayed(), `Expected success alert to be visible after registration`)
+        
+        const alertText = alert.getText()
+        const expectedText = 'Добро пожаловать на GoodiDeas!'
+        assert(alertText.includes(expectedText),
+            `Alert text: "${alertText}" to match expected: "${expectedText}", after success registration`)
+        
+    })
+})
--- test/specs/smoke.ts
+++ test/specs/smoke.ts
@@ -1,9 +1,10 @@
 describe('Website', function () {
-    it('should be alive', function () {
+    it('should have the box', function () {
         browser.url(`/`)
-        const img = $('img[src="https://agoodideas.herokuapp.com/assets/logo_s2-68f9fecbca0ae1ccdf8d4109d8d656b5f8a235f68cb6fd9adccea5a444f6d7c9.png"]')
-        if(!img.isExisting()) {
-            throw new Error('Website should be opened, and logo displayed');
+        const box = $('.box.panel')
+        if(!box.isDisplayed()) {
+            throw new Error ('box should be displayed');
         }
     })
 })
+
